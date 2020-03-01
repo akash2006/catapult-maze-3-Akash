c923fa4c880bafd80ee3cd426438f3be3fdb841d
akash2006 <59837849+akash2006@users.noreply.github.com>
2020-03-01 19:07:19 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,19 @@
+class Ball{
+    constructor(x,y,radius){
+        var options = {
+            'restitiution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.circle(x,y,radius,options);
+        this.radius = radius;
+        
+        World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        fill(224, 223, 117);
+        ellipseMode(CENTER);
+        ellipse(pos.x,pos.y,this.radius);
+    }
+}
